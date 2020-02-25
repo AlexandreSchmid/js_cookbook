a0850cb8eed9c6c6f9a87a14a846f8383f46b2fa
AlexandreSchmid <alexandre.schmid@neuf.fr>
2020-02-25 22:48:49 +0100

    Setting autocompletion and JSON informations

--- js/cookBook.js
+++ js/cookBook.js
@@ -1,49 +1,52 @@
-getRecipe('https://raw.githubusercontent.com/LeaVerou/forkgasm/master/recipes.json', function(response) {
-    // Getting JSON data and parsing, create arrays
-    var res = JSON.parse(response)
-    createView(res)
-})
-
-function createView(res) {
-    var cookInfosArray = {}
-    var ingredients = []
-    var tags = []
-    var instructions = []
-    var recipe = []
-    recipe.push(cookInfosArray, ingredients, tags, instructions)
-    var cookInfos = ['name', 'id', 'image', 'notes']
-
-    cookInfos.forEach(el => {
-        cookInfosArray[el] = res.recipe[0][el]
-    })
-
-    res.recipe[0].ingredient.forEach(element => {
-        ingredients.push(element)
-    })
-
-    res.recipe[0].step.forEach(element => {
-        instructions.push(element)
-    })
-
-    res.recipe[0].tag.forEach(el => {
-        tags.push(el)
-    })
-    document.querySelector("section h1").innerHTML = recipe[0].name
-
-    recipe[1].forEach(el => {
+function createView(item) {
+    document.querySelector("section h1").innerHTML = item.name
+    document.getElementById("small").innerHTML = item.description
+    item.ingredient.forEach(el => {
         var li = document.createElement('li')
         var sentence = ''
-        if (el.amount != '') {
-            sentence += el.amount + ' '
-        }
-        if (el.unit != '') {
-            sentence += el.unit
+        if (el.amount || el.unit) {
+            sentence = el.amount + ' ' + el.unit
         }
         li.innerHTML = el.name + ' ' + sentence
         document.querySelector("section .ingredients").appendChild(li)
     })
-    recipe[3].forEach(el => {
-        document.querySelector("section p").innerHTML += el.description
+    item.step.forEach(el => {
+        document.querySelector("section p").append(el.description)
     })
-    document.getElementById('img').setAttribute('src', recipe[0].image)
+    if (item.tag) {
+        document.querySelector('.note').innerHTML = item.tag
+    }
+    document.getElementById('img').setAttribute('src', item.image)
 }
+
+getRecipe('https://raw.githubusercontent.com/LeaVerou/forkgasm/master/recipes.json', function(response) {
+    // Getting JSON data and parsing, create arrays
+    const res = JSON.parse(response)
+    document.querySelector('#sB').addEventListener("input", function(e, res) {
+        var listRecipe = []
+        res.recipe.forEach(el => {
+            listRecipe.push(el.name)
+        })
+        var opt = document.createElement('option')
+        listRecipe.forEach(el => {
+            if (el.includes(e.target.value)) {
+                opt.value = el
+                document.getElementById("recipeList").appendChild(opt)
+            } else {
+                opt.innerHTML = ''
+            }
+        })
+    })
+    document.querySelector('form').addEventListener('submit', function(e) {
+        e.preventDefault()
+        if (document.querySelector('#sB').value) {
+            var i = 0
+            res.recipe.forEach(el => {
+                if (el.name === document.querySelector('#sB').value) {
+                    createView(res.recipe[i])
+                }
+                i++
+            })
+        }
+    })
+})
