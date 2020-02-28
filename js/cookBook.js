fffebca6343cee99e4313ef6114b676ec164e806
AlexandreSchmid <alexandre.schmid@neuf.fr>
2020-02-28 16:43:09 +0100

    menu setting

--- js/cookBook.js
+++ js/cookBook.js
@@ -1,6 +1,7 @@
 function createView(item) {
     document.querySelector("section h1").innerHTML = item.name
     document.getElementById("small").innerHTML = item.description
+    document.querySelector('ul').innerHTML = ''
     item.ingredient.forEach(el => {
         var li = document.createElement('li')
         var sentence = ''
@@ -10,11 +11,19 @@ function createView(item) {
         li.innerHTML = el.name + ' ' + sentence
         document.querySelector("section .ingredients").appendChild(li)
     })
+    document.querySelector("section p").innerHTML = ''
     item.step.forEach(el => {
         document.querySelector("section p").append(el.description)
     })
     if (item.tag) {
-        document.querySelector('.note').innerHTML = item.tag
+        document.querySelector('.note').innerHTML = ''
+        item.tag.forEach(el => {
+            var a = document.createElement('a')
+            var aText = document.createTextNode(el + ' ')
+            a.href = el
+            a.appendChild(aText)
+            document.querySelector('.note').append(a)
+        })
     }
     document.getElementById('img').setAttribute('src', item.image)
 }
