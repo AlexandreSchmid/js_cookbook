cb0aad38a8e59b0fbbd31354cbae054a42701fd0
AlexandreSchmid <alexandre.schmid@neuf.fr>
2020-03-01 20:10:07 +0100

    finish the json parsing

--- js/cookBook.js
+++ js/cookBook.js
@@ -1,25 +1,42 @@
 function createView(item) {
-    document.querySelector("section h1").innerHTML = item.name
+    document.querySelector(".recipeTitle").innerHTML = item.name
     document.getElementById("small").innerHTML = item.description
     document.querySelector('ul').innerHTML = ''
+    let unitAmount = function(it1, it2, it3) {
+        let sentence = ''
+        if (it1) { sentence += it1 + ' ' }
+        if (it2) { sentence += it2 + ' ' }
+        if (it3) { sentence += it3 + ' ' }
+        return sentence
+    }
     item.ingredient.forEach(el => {
-        var li = document.createElement('li')
-        var sentence = ''
-        if (el.amount || el.unit) {
-            sentence = el.amount + ' ' + el.unit
-        } else { sentence = "" }
-        li.innerHTML = sentence + ' ' + el.name
+        let li = document.createElement('li')
+        li.innerHTML = unitAmount(el.amount, el.unit, el.preparation) + el.name
         document.querySelector("section .ingredients").appendChild(li)
     })
+    if (item.ingredientGroup) {
+        item.ingredientGroup.forEach(el => {
+            let li = document.createElement('li')
+            li.innerHTML = '<b>' + el.name + '</b>'
+            document.querySelector("section .ingredients").appendChild(li)
+            el.ingredient.forEach(el => {
+                let liSmall = document.createElement('li')
+                liSmall.innerHTML = unitAmount(el.amount, el.unit, el.preparation) + el.name
+                document.querySelector("section .ingredients").appendChild(liSmall)
+            })
+        })
+    }
     document.querySelector("section p").innerHTML = ''
     item.step.forEach(el => {
+        let hr = document.createElement("hr")
+        document.querySelector("section p").append(hr)
         document.querySelector("section p").append(el.description)
     })
     if (item.tag) {
         document.querySelector('.note').innerHTML = ''
         item.tag.forEach(el => {
-            var a = document.createElement('a')
-            var aText = document.createTextNode(el + ' ')
+            let a = document.createElement('a')
+            let aText = document.createTextNode(el + ' ')
             a.href = el
             a.appendChild(aText)
             document.querySelector('.note').append(a)
@@ -33,25 +50,25 @@ function openModal(itemArray, typeInput) {}
 getRecipe('https://raw.githubusercontent.com/LeaVerou/forkgasm/master/recipes.json', function(response) {
     // Getting JSON data and parsing, create arrays
     const res = JSON.parse(response)
-    document.querySelector('#sB').addEventListener("input", function(e, res) {
-        const listRecipe = []
+    const listRecipe = []
+    document.querySelector('#sB').addEventListener("input", function(e) {
         res.recipe.forEach(el => {
             listRecipe.push(el.name)
         })
-        var opt = document.createElement('option')
+        let opt = document.createElement('option')
         listRecipe.forEach(el => {
-            if (el.includes(e.target.value)) {
+            if (el.includes(e.target.value) === true) {
                 opt.value = el
                 document.getElementById("recipeList").appendChild(opt)
             } else {
-                opt.innerHTML = ''
+                document.getElementById("recipeList").removeChild(opt)
             }
         })
     })
     document.querySelector('form').addEventListener('submit', function(e) {
         e.preventDefault()
         if (document.querySelector('#sB').value) {
-            var i = 0
+            let i = 0
             res.recipe.forEach(el => {
                 if (el.name === document.querySelector('#sB').value) {
                     createView(res.recipe[i])
@@ -63,10 +80,9 @@ getRecipe('https://raw.githubusercontent.com/LeaVerou/forkgasm/master/recipes.js
 
     for (var i = 0; i < document.getElementsByName("bottomMenu").length; i++) {
         document.getElementsByName("bottomMenu")[i].addEventListener("click", function(e) {
-            var tag = e.target.href
+            let tag = e.target.href
             switch (tag) {
                 case "#all":
-                    console.log(listRecipe)
                     openModal(listRecipe, "checkbox")
                 case "#byNutri":
                     break
