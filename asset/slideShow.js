f76fea221c598a4d3d111a24d0dcfd22a8dd2bd5
AlexandreSchmid <alexandre.schmid@neuf.fr>
2020-03-19 22:50:13 +0100

    setting slideShow's interactions

--- asset/slideShow.js
+++ asset/slideShow.js
@@ -1,15 +1,5 @@
 function carousel(res) {
-    let images = {}
     let carousel = document.querySelector('.slideShow')
-        // Array of images url and recipe associate
-    let j = 0
-    res.recipe.forEach(el => {
-        if (el.image.length !== 0 && j <= 3) {
-            images[el.name] = el.image
-            j++
-        }
-    });
-    console.log(images)
     carousel.style.display = 'inline-flex'
         // Create elements for display images and recipe's names
     for (let i = 0; i < 4; i++) {
@@ -18,15 +8,26 @@ function carousel(res) {
         let sizeX = '350px',
             sizeY = '350px'
         elt[0].setAttribute('class', 'slide')
-        elt[1].setAttribute('src', images[i.name])
+        elt[1].setAttribute('src', res.recipe[i].image)
         elt[1].style.width = sizeX
         elt[1].style.height = sizeY
         elt[1].style.borderRadius = "30px"
         elt[1].style.margin = "20px"
-        elt[1].setAttribute('alt', images[i.image])
+        elt[1].name = 'recipeImg'
+        elt[1].id = res.recipe[i].name
         elt[2].appendChild(elt[1])
         elt[2].href = '#'
         elt[0].appendChild(elt[2])
         carousel.appendChild(elt[0])
+    }
+
+    let img = document.getElementsByName('recipeImg')
+    for (let i = 0; i < img.length; i++) {
+        img[i].addEventListener('click', function(e) {
+            if (e.target.id === res.recipe[i].name) {
+                document.querySelector('body .slideShow').remove()
+                createView(res.recipe[i])
+            }
+        })
     }
 }
