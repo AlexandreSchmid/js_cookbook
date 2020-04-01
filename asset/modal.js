58313581cc325396f957e2a26fa3f175220e1aa7
AlexandreSchmid <alexandre.schmid@neuf.fr>
2020-04-01 21:26:14 +0200

    Modifications 2

--- asset/modal.js
+++ asset/modal.js
@@ -4,15 +4,15 @@ document.querySelector('.close').addEventListener('click', function() {
         document.querySelector('.modal-content').style.display = 'none'
     })
     // Open and setting the modal box at screen
-function openModal(itemArray, typeData, cook) {
+function openModal(itemArray, typeData) {
     document.querySelector('.modal').style.display = 'block'
     document.querySelector('.modal-content').style.display = 'block'
     document.querySelector('.modal-content .content').innerHTML = ''
         // Functions to display the data
     if (typeData === 'all') {
         allRecipes(itemArray)
-    } else if (typeData === 'ingredient' || typeData === 'tag') {
-        filter(itemArray, typeData, cook)
+    } else if (typeData === 'byIngredient' || typeData === 'byTag') {
+        filter(itemArray, typeData)
     }
 }
 // Lists all the recipes that are available
@@ -39,18 +39,16 @@ function allRecipes(itemArray) {
 // Create buttons on the left and show the recipes on the right
 function filter(itemArray, typeData) {
     let content = document.querySelector('.modal-content .content')
-        // div that contains all buttons
-    let divButtons = document.createElement('div')
-    divButtons.setAttribute('class', 'verticalTab')
-        // div that contains recipes to display
-    let divRecipes = document.createElement('div')
-    divRecipes.setAttribute('class', 'tabRecipes')
-        // Title of modal box
-    let p = document.createElement('p')
-    p.setAttribute('class', 'title')
-    divRecipes.append(p.innerHTML = `Filter recipes by ${typeData}`)
-    content.append(divButtons)
-    content.append(divRecipes)
+        // div that contain all buttons
+    let divAllButtons = ['div', 'div', 'p']
+    let divAllButtonsAttributes = ['verticalTab', 'tabRecipes', 'title']
+    let elts = divAllButtons.map(x => document.createElement(x))
+    for (i in divAllButtonsAttributes) {
+        elts[i].setAttribute('class', divAllButtonsAttributes[i])
+    }
+    elts[1].append(elts[2].innerHTML = `Filter recipes by ${typeData}`)
+    content.append(elts[0])
+    content.append(elts[1])
     for (let i in itemArray) {
         // normalized for the tabs and the id of each recipe displayed
         // Remove spaces and special characters for selector
@@ -60,50 +58,47 @@ function filter(itemArray, typeData) {
         button.setAttribute('id', i)
         button.setAttribute('onclick', `displayRecipes(event, '${normalized}')`)
         button.innerHTML = i
-        divButtons.append(button)
+        elts[0].append(button)
             // one div per category of recipe (ingredient or tag)
         let itemRecipe = document.createElement('div')
         itemRecipe.setAttribute('class', 'tabContent')
         itemRecipe.id = normalized
             // create one link per recipe
         let table = itemArray[i].split(',')
+        table.splice(0, 1) //Removes undefined values
         table.forEach(el => {
             let p = document.createElement('p')
             let a = document.createElement('a')
-            if (el != 'undefined') {
-                a.href = '#'
-                a.name = "recipeList"
-                a.id = el
-                a.innerHTML = el
-                p.append(a)
-            }
+            a.href = '#'
+            a.name = "recipeList"
+            a.id = el
+            a.innerHTML = el
+            p.append(a)
             itemRecipe.append(p)
         })
-        divRecipes.append(itemRecipe)
+        elts[1].append(itemRecipe)
     }
 }
-
 // Creates the view for one recipe
-function recipeAll(res, cook) {
+function recipeAll(res) {
     for (let i = 0; i < res.recipe.length; i++) {
         document.getElementsByName('recipe')[i].addEventListener('click', function(e) {
             if (e.currentTarget.id === res.recipe[i].name) {
-                cook.createView(res.recipe[i])
+                cookbook.createView(res.recipe[i])
             }
             // Close the modal box after
             document.querySelector('.close').click()
         })
     }
 }
-
 // Search by tag or by ingredient, detect the click and creates the view for one recipe
-function recipeByTag(res, cook) {
+function recipeByTag(res) {
     let items = document.querySelectorAll('.tabRecipes .tabContent p a')
     for (let i = 0; i < items.length; i++) {
         items[i].addEventListener('click', function() {
             res.recipe.forEach(el => {
                 if (el.name === items[i].id) {
-                    cook.createView(el)
+                    cookbook.createView(el)
                 }
             })
             document.querySelector('.close').click()
@@ -117,7 +112,6 @@ function displayRecipes(e, idButton) {
     for (i = 0; i < tabcontent.length; i++) {
         tabcontent[i].style.display = "none"
     }
-
     tablinks = document.getElementsByClassName('tabLink')
     for (i = 0; i < tablinks; i++) {
         tablinks[i].className = tablinks[i].className.replace(' active', '')
--- js/cookBook.js
+++ js/cookBook.js
@@ -1,21 +1,62 @@
-class Cookbook {
-    constructor(listRecipe, res) {
-        this.listRecipe = listRecipe
-        this.res = res
-    }
-
+var cookbook = {
+    carousel(res) {
+        // At loading of the main page, make sure no recipe is displayed
+        if (document.getElementById('img').getAttribute('src').length == 0) {
+            // Hide all the other elements for the slideshow, then starts the slideshow
+            document.querySelector('.container').style.display = 'none'
+                // Setting slideShow
+            let carousel = document.querySelector('.slideShow')
+            carousel.style.display = 'inline-flex'
+                // Create elements for display images and recipe's names
+            for (let i = 0; i < 4; i++) {
+                // Random recipe's image link 
+                let j = Math.round(res.recipe.length * Math.random())
+                let elts = ['div', 'img', 'a']
+                let elt = elts.map(x => document.createElement(x))
+                elt[0].setAttribute('class', 'slide')
+                elt[1].setAttribute('src', res.recipe[j].image)
+                elt[1].name = 'recipeImg'
+                elt[1].id = j
+                elt[2].appendChild(elt[1])
+                elt[2].href = '#'
+                elt[0].appendChild(elt[2])
+                carousel.appendChild(elt[0])
+            }
+            // Click listener when click on an image
+            let img = document.getElementsByName('recipeImg')
+            img.forEach(el => {
+                    el.addEventListener('click', function() {
+                        cookbook.createView(res.recipe[el.id])
+                    })
+                })
+                // Mouseover / mouseout recipe's name 
+            img.forEach(el => {
+                el.addEventListener('mouseover', function(e) {
+                    let span = document.createElement('span')
+                    span.innerHTML = res.recipe[el.id].name
+                    el.parentNode.appendChild(span)
+                })
+                el.addEventListener('mouseout', function(e) {
+                    document.querySelector('.slideShow .slide span').remove()
+                })
+            })
+        }
+    },
     // Autocomplete input field
-    autocomplete() {
+    autocomplete(listRecipe) {
         document.querySelector('#sB').value = ''
-        this.listRecipe.forEach(el => {
+        listRecipe.forEach(el => {
             let opt = document.createElement('option')
             opt.value = el
             document.getElementById('recipeList').appendChild(opt)
         })
-    }
-
+    },
     // Create the recipe page
     createView(res) {
+        // Removes the last content 
+        let lastContent = ['ul', 'section p', '.note']
+        lastContent.map(x => document.querySelector(x).innerHTML = '')
+
         // Removes the slideshow and set the container for recipes
         if (document.querySelector('body .slideShow')) {
             document.querySelector('body .slideShow').remove()
@@ -23,7 +64,6 @@ class Cookbook {
         }
         document.querySelector('.recipeTitle').innerHTML = res.name // name of the recipe
         document.getElementById('description').innerHTML = res.description // desciption of the recipe
-        document.querySelector('ul').innerHTML = ''
 
         //*     INGREDIENTS TABLE       */
         // Each ingredient don't need a preparation or unit (sometimes)
@@ -63,9 +103,7 @@ class Cookbook {
                 })
             })
         }
-        // Instructions of recipe
-        document.querySelector('section p').innerHTML = ''
-        console.log(res.step)
+        // Instructions of the recipes
         res.step.forEach(el => {
                 let hr = document.createElement('hr')
                 document.querySelector('section p').append(hr)
@@ -73,7 +111,6 @@ class Cookbook {
             })
             // Tags for each recipe
         if (res.tag) {
-            document.querySelector('.note').innerHTML = ''
             res.tag.forEach(el => {
                 let a = document.createElement('a')
                 let aText = document.createTextNode('#' + el + ' ')
@@ -85,42 +122,38 @@ class Cookbook {
         document.getElementById('img').style.display = 'inline'
         document.getElementById('img').setAttribute('src', res.image) // Image of dish
         document.getElementById('img').style.border = '2px solid white'
-    }
-}
-
-function submitRecipe(res, cook) {
-    // By submitting a search, creating the view for one recipe
-    document.querySelector('form').addEventListener('submit', function(e) {
-        e.preventDefault()
-        if (document.querySelector('#sB').value) {
-            let i = 0
-            res.recipe.forEach(el => {
-                if (el.name === document.querySelector('#sB').value) {
-                    cook.createView(res.recipe[i])
+    },
+    submitRecipe(res) {
+        // By submitting a search, creating the view for one recipe
+        document.querySelector('form').addEventListener('submit', function(e) {
+            e.preventDefault()
+            if (document.querySelector('#sB').value) {
+                let i = 0
+                res.recipe.forEach(el => {
+                    if (el.name === document.querySelector('#sB').value) {
+                        cookbook.createView(res.recipe[i])
+                    }
+                    i++
+                })
+            }
+            document.querySelector('#sB').value = ''
+        })
+    },
+    bottonMenu(listTag, listIngredients, listRecipe, res) {
+        // Bottom menu, by clicking, open modalboxes (in a separate file)
+        for (let i = 0; i < document.getElementsByName('bottomMenu').length; i++) {
+            document.getElementsByName('bottomMenu')[i].addEventListener('click', function(e) {
+                let tag = e.target.parentNode.parentNode.parentNode.className
+                let params = {
+                    'all': listRecipe,
+                    'byIngredient': listIngredients,
+                    'byTag': listTag
                 }
-                i++
+                openModal(params[tag], tag)
+                if (params[tag] === listRecipe) recipeAll(res)
+                recipeByTag(res)
             })
         }
-        document.querySelector('#sB').value = ''
-    })
-}
-
-function bottonMenu(listTag, listIngredients, listRecipe, cook, res) {
-    // Bottom menu, by clicking, open modalboxes (in a separate file)
-    for (let i = 0; i < document.getElementsByName('bottomMenu').length; i++) {
-        document.getElementsByName('bottomMenu')[i].addEventListener('click', function(e) {
-            let tag = e.target.href
-            if (tag.includes('#all')) {
-                openModal(listRecipe, 'all', res) // call for the modal box
-                recipeAll(res, cook) // Display into the modal box
-            } else if (tag.includes('#byIngredient')) {
-                openModal(listIngredients, 'ingredient', res)
-                recipeByTag(res, cook)
-            } else if (tag.includes('#byTag')) {
-                openModal(listTag, 'tag', res)
-                recipeByTag(res, cook)
-            }
-        })
     }
 }
 
@@ -152,16 +185,9 @@ getRecipe('https://raw.githubusercontent.com/LeaVerou/forkgasm/master/recipes.js
             })
         }
     })
-    var cook = new Cookbook(listRecipe, res)
-        // At loading of the main page, make sure no recipe is displayed
-    if (document.getElementById('img').getAttribute('src').length == 0) {
-        // Hide all the other elements for the slideshow, then starts the slideshow
-        document.querySelector('.container').style.display = 'none'
-        carousel(res, cook)
-    }
 
-    bottonMenu(listTag, listIngredients, listRecipe, cook, res)
-
-    cook.autocomplete()
-    submitRecipe(res, cook)
+    cookbook.carousel(res)
+    cookbook.autocomplete(listRecipe)
+    cookbook.submitRecipe(res)
+    cookbook.bottonMenu(listTag, listIngredients, listRecipe, res)
 })
